4b07b229966b05496a24b14ad5ea48907e0eaec4
Susan Minott <susan_minott@yahoo.com>
2022-06-08 10:32:53 -0400

    commit tree 112a6bbabf986a2691350f54a79d5ba2d156aaad

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,35 +1,40 @@
 def create_an_empty_array
-  
+  arr = Array.new
+  # Could have written arr = []
 end
 
 def create_an_array
-  
+  arr = Array.new(4)
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  array << element
+  #Could have written array.push(element)
 end
 
 def add_element_to_start_of_array(array, element)
-  
+  array.insert(0, element)
+  #Could have written array.unshift(element)
 end
 
 def remove_element_from_end_of_array(array)
-  
+  #Could have writtem array.pop
+  array.delete_at(-1)
 end
 
 def remove_element_from_start_of_array(array)
-  
+  #Could have written array.shift
+  array.delete_at(0)
 end
 
 def retrieve_element_from_index(array, index_number)
-  
+  array.delete_at(index_number)
 end
 
 def retrieve_first_element_from_array(array)
-  
+  array[0]
 end
 
 def retrieve_last_element_from_array(array)
-  
+  array[-1]
 end
